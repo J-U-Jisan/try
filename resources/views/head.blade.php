ff3a76e41ab1d9ec04aa9f866443445848e697c3
J.U.Jisan <jisan.cse16@gmail.com>
2020-12-08 00:42:31 +0600

    google indexing

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="google-site-verification" content="2Z2M_-afkV2N2gbf4AGrvhjgOIvoSEoYm7Z9SRJnvgo" />
     <!-- Primary Meta Tags -->
     <title>@yield('title') Try</title>
     <meta name="title" content="@yield('meta_title','Try')">
